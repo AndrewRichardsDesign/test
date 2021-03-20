6ea9f5e9596d67925247ab74defe7777e1e854ed
AndrewRichardsDesign <andrewrichards@Andrews-MacBook-Pro.local>
2021-03-20 01:10:24 -0700

    update

--- app/js/script.js
+++ app/js/script.js
@@ -173,7 +173,7 @@ window.onload = function() {
   }
   // INJECT CSS
   var css = document.createElement("style");
-  css.type = "text/css";
+  css.nodeType = "text/css";
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
